e714ef6083bee796ff7ea77df56a3537ff5aa54d
Tuanemendes <tuane.belmiro@gmail.com>
2022-05-19 14:02:00 -0300

    Atividade de número 3

---
+++ tuanembex3/src/tuanembex3/Menu.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tuanembex3;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author tuane
+ */
+public class Menu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame janela = new JanelaPrincipal();
+        janela.setVisible(true);
+        janela.setLocationRelativeTo(null);
+        janela.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
+    
+}
